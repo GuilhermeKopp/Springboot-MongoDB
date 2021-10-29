2d380e1e2c106bfe27d762220594c5e3cadf1f64
GuilhermeKopp <guikopp19@gmail.com>
2021-10-29 12:40:58 -0300

    Conectando ao MongoDB

---
+++ src/main/java/com/guilhermekopp/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.guilhermekopp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guilhermekopp.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
